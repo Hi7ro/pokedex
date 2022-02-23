ebb7ac467061842c0d82e90ee1f31c112b1e57c7
HW <a.k_windsor@protonmail.com>
2022-02-23 12:29:27 +0100

    added color + clickable

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ let currentPokemon;
 let pokemonCollection = ['bulbasaur', 'ivysaur', 'venusaur', 'charmander', 'charmeleon', 'charizard', 'squirtle', 'wartortle', 'blastoise', 'pikachu'];
 let pokemonAsJson = [];
 
-
-
 async function loadPokemon() {
   for (let i = 0; i < pokemonCollection.length; i++) {
-    let collectible = pokemonCollection[i];
-    let url = `https://pokeapi.co/api/v2/pokemon/${collectible}`
+    let pokemon = pokemonCollection[i];
+    let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
     let response = await fetch(url);
     currentPokemon = await response.json();
     pokemonAsJson.push(currentPokemon);
     console.log(currentPokemon);
+    renderPokemonInfo(i);
+    colors(i);
   }
   renderCard();
   renderPokedex();
@@ -21,29 +21,31 @@ async function loadPokemon() {
 
 function renderCard() {
   let pokedex = document.getElementById('pokedex');
-  let pokeInfo = document.getElementById('pokemonInfo');
-
   pokedex.innerHTML = `
-      <h1>${currentPokemon['name']}</h1>
-      <div class="elementBox">${currentPokemon['types'][0]['type']['name']}</div>
-      <img src="${currentPokemon['sprites']['other']['home']['front_default']}">
+      <h1 id="title">${currentPokemon['name']}</h1>
+      <div id="element" class="elementBox">${currentPokemon['types'][0]['type']['name']}</div>
+      <img id="mainCardImg" src="${currentPokemon['sprites']['other']['home']['front_default']}">
       `
-  pokeInfo.innerHTML = '<ul id="stat-list"></ul>';
+}
 
-  for (let i = 0; i <= 5; i++) {
+function renderPokemonInfo(i) {
+  let pokeInfo = document.getElementById('pokemonInfo');
+  pokeInfo.innerHTML = '<ul id="stat-list"></ul>';
+  for (let j = 0; j <= 5; j++) {
     document.getElementById('stat-list').innerHTML += `
-        <li>${currentPokemon['stats'][i]['stat']['name']}: <b>${currentPokemon['stats'][i]['base_stat']}</b> <div class="skill-bar" id="skill-bar" style="width: ${currentPokemon['stats'][i]['base_stat']}%"></div></li>
+        <li>${pokemonAsJson[i]['stats'][j]['stat']['name']}: 
+        <b>${pokemonAsJson[i]['stats'][j]['base_stat']}</b> 
+        <div class="skill-bar" id="skill-bar" style="width: 
+        ${pokemonAsJson[i]['stats'][j]['base_stat']}%"></div></li>
         `;
   }
-
 }
 
 function renderPokedex() {
   let pokedex = document.getElementById('pokedexBox');
   for (let i = 0; i < pokemonCollection.length; i++) {
-
     pokedex.innerHTML += `
-    <div class="pokemonCards" onclick="showPokemon()">
+    <div class="pokemonCards color-${i}" onclick="showPokemonCard(${i})">
       <div>
         <h2>${pokemonAsJson[i]['name']}</h2>
         <div class="elementBox">${pokemonAsJson[i]['types'][0]['type']['name']}</div>
@@ -51,22 +53,24 @@ function renderPokedex() {
       </div>
     </div>
     `;
-
   }
-
 }
 
-function showPokemon(i) {
+function showPokemonCard(i) {
+  let h1 = document.getElementById('title');
+  let element = document.getElementById('element');
+  let cardImg = document.getElementById('mainCardImg');
+  h1.innerHTML = `${pokemonAsJson[i]['name']}`
+  element.innerHTML = `${pokemonAsJson[i]['types'][0]['type']['name']}`
+  cardImg.src = `${pokemonAsJson[i]['sprites']['other']['home']['front_default']}`
 
+  document.getElementById('pokedex').classList.add = `color-${i}`;
+
+  renderPokemonInfo(i)
 }
 
-// function skinColors() {
-//   //TODO Not Working!
-//   let stats = currentPokemon['stats'][i]['base_stat']
-//   if (stats < 50) {
-//     document.getElementById('skill-bar').style.backgroundColor = 'black'
-//   }
-//   if (stats > 50) {
-//     document.getElementById('skill-bar').style.backgroundColor = 'green'
-//   }
-// }
+function colors(i) {
+
+
+
+}
